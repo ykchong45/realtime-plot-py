84e0cff888228788e9401ca157627f9628a7ad1c
ykchong45 <ykchong45@gmail.com>
2023-11-22 17:09:29 +0900

    feat: input x_delta as input to horizontal axis

--- realtime-scatter.py
+++ realtime-scatter.py
@@ -4,14 +4,15 @@ import time
 import random
 
 # configurables
-buffer_size= 1000    # number of data in across the horizontal axis 
+buffer_size= 2000    # number of data in across the horizontal axis 
 fpsTestSize = 1000  # every `fpsTestSize` of frames we re-evaluate the fps
 fpsReportRate = 100 # every `fpsReportRate` count of frames, the program report the fps once
 
 # START
-x = np.linspace(0, 10, buffer_size)
+x = np.zeros(buffer_size)
 y = np.zeros(buffer_size)
 fig, ax = plt.subplots()
+ax.set_xlim([0, buffer_size])
 ax.set_ylim([-1, 1])
 sc = ax.scatter(x, y, animated=True)
 
@@ -38,14 +39,19 @@ for j in range(10000):
 
 
     # generate data and shift
-    # newXData = random.uniform(0.8, 1.2)
+    newXDelta = random.uniform(0, 2)
     newYData = np.sin(j * 2 * np.pi / buffer_size)
+
     if j < buffer_size:
+        x[j] = x[j-1] + newXDelta
         y[j] = newYData
     else:
-        # insertIdx = j % buffer_size
+        x -= x[1]
+        x = np.roll(x, -1)
+        x[-1] = x[-2] + newXDelta
         y = np.roll(y, -1)
         y[-1] = newYData
+
     
     sc.set_offsets(np.column_stack((x, y)))
     ax.draw_artist(sc)
