43c4763115720f5c3cca474dcc207c951ded4666
ykchong45 <ykchong45@gmail.com>
2023-11-22 17:09:29 +0900

    refactor: modularize the code

--- realtime-scatter.py
+++ realtime-scatter.py
@@ -1,69 +1,94 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-import random
+import math
 
-# configurables
-buffer_size= 2000    # number of data in across the horizontal axis 
-fpsTestSize = 1000  # every `fpsTestSize` of frames we re-evaluate the fps
-fpsReportRate = 100 # every `fpsReportRate` count of frames, the program report the fps once
+class DataGenerator:
+    def __init__(self):
+        self.x = 0
 
-# START
-x = np.zeros(buffer_size)
-y = np.zeros(buffer_size)
-fig, ax = plt.subplots()
-ax.set_xlim([0, buffer_size])
-ax.set_ylim([-1, 1])
-sc = ax.scatter(x, y, animated=True)
-
-plt.title("Sine Wave Scatter Plot")
-plt.show(block=False)
-plt.pause(0.1)
-
-bg = fig.canvas.copy_from_bbox(ax.get_figure().bbox)
-
-ax.draw_artist(sc)
-
-fig.canvas.blit(fig.clipbox)
-old_fig_size = fig.get_size_inches()
-
-tStart = 0
-fpsTestStartFrame = 0
-
-for j in range(10000):
-    ## RESPONSE TO RESIZING THE WINDOW
-    if (old_fig_size != fig.get_size_inches()).any():
-        bg = fig.canvas.copy_from_bbox(fig.bbox)   
-        old_fig_size = fig.get_size_inches()
-    fig.canvas.restore_region(bg)
+    def output_data(self):
+        self.x += 1
+        y = math.sin(self.x * 2 * math.pi / 1000)
+        return self.x, y
 
 
-    # generate data and shift
-    newXDelta = random.uniform(0, 2)
-    newYData = np.sin(j * 2 * np.pi / buffer_size)
+class RealTimeScatterPlot:
+    def __init__(self, data_source, buffer_size=1000, fps_sample_size=1000, fps_report_rate=100, xlim=[-1000, 10], ylim=[-1, 1]):
+        self.data_source = data_source
+        self.buffer_size = buffer_size
+        self.fps_sample_size = fps_sample_size
+        self.fps_report_rate = fps_report_rate
 
-    if j < buffer_size:
-        x[j] = x[j-1] + newXDelta
-        y[j] = newYData
-    else:
-        x -= x[1]
-        x = np.roll(x, -1)
-        x[-1] = x[-2] + newXDelta
-        y = np.roll(y, -1)
-        y[-1] = newYData
+        self.x = np.zeros(buffer_size)
+        self.y = np.zeros(buffer_size)
+        self.last_horizontal_tick = 0
 
-    
-    sc.set_offsets(np.column_stack((x, y)))
-    ax.draw_artist(sc)
-    fig.canvas.blit(fig.clipbox)
-    fig.canvas.flush_events()
-    
-    # calculate fps with test range
-    if j % fpsTestSize == 0:
-        tStart = time.time()
-        fpsTestStartFrame = j
-    if j % fpsReportRate == 0:
-        elapsedTime = time.time() - tStart
-        if elapsedTime:
-            fps = (j - fpsTestStartFrame) / elapsedTime
-            print(fps)
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_xlim(xlim)
+        self.ax.set_ylim(ylim)
+        self.sc = self.ax.scatter(self.x, self.y, animated=True, s=1)
+
+        plt.title("Realtime Scatter Plot")
+        plt.show(block=False)
+        plt.pause(0.1)
+
+        self.bg = self.fig.canvas.copy_from_bbox(self.ax.get_figure().bbox)
+        self.ax.draw_artist(self.sc)
+        self.fig.canvas.blit(self.fig.clipbox)
+        self.old_fig_size = self.fig.get_size_inches()
+
+        # fps calculation
+        self.t_start = 0
+        self.fps_sample_start_frame = 0
+
+    def calculate_fps(self, i):
+        if i % self.fps_sample_size == 0:
+            self.t_start = time.time()
+            self.fps_sample_start_frame = i
+        if i % self.fps_report_rate == 0:
+            elapsed_time = time.time() - self.t_start
+            if elapsed_time:
+                fps = (i - self.fps_sample_start_frame) / elapsed_time
+                print("fps: {}".format(fps))
+        
+    def update_plot(self, data_point_x, data_point_y, i):
+        if (self.old_fig_size != self.fig.get_size_inches()).any():
+            self.bg = self.fig.canvas.copy_from_bbox(self.fig.bbox)
+            self.old_fig_size = self.fig.get_size_inches()
+        self.fig.canvas.restore_region(self.bg)
+
+        # recalculate shifted horizontal value
+        if not self.last_horizontal_tick:
+            new_x_delta = 0
+        else:
+            new_x_delta = (data_point_x - self.last_horizontal_tick)
+        self.last_horizontal_tick = data_point_x
+
+        new_y_data = data_point_y
+
+        # left shift the buffer
+        self.x -= self.x[1]
+        self.x = np.roll(self.x, -1)
+        self.x[-1] = self.x[-2] + new_x_delta
+        self.x -= self.x[-1]    # make the last item to be 0, and all rest to be negative
+        self.y = np.roll(self.y, -1)
+        self.y[-1] = new_y_data
+
+        self.sc.set_offsets(np.column_stack((self.x, self.y)))
+        self.ax.draw_artist(self.sc)
+        self.fig.canvas.blit(self.fig.clipbox)
+        self.fig.canvas.flush_events()
+
+        self.calculate_fps(i)
+
+    def run(self, max_iterations=10000):
+        for i in range(max_iterations):
+            newDataTime, data_point_y = self.data_source.output_data()
+            self.update_plot(newDataTime, data_point_y, i)
+
+# Usage
+if __name__ == "__main__":
+    data_generator = DataGenerator()
+    real_time_plot = RealTimeScatterPlot(data_generator)
+    real_time_plot.run()
